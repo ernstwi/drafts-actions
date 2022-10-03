72ce25e77d870d512d256643a12eebed48201dcb
Ernst Widerberg <ernstwi@kth.se>
2022-10-03 17:27:33 +0200

    tag-overview: Add styling

--- tag-overview.js
+++ tag-overview.js
@@ -8,15 +8,37 @@ let data = {
     tags: tags.map(x => ({ name: x, content: Draft.query('', 'inbox', [ x ]).map(y => y.title)}))
 }
 
-let template = `{{#tags}}
-<h2>{{name}}</h2>
-
-<ul>
-    {{#content}}
-    <li>{{.}}</li>
-    {{/content}}
-</ul>
-{{/tags}}
-`;
+let template = `<html>
+    <head>
+        <style>
+            * {
+                font-family: 'Monaco';
+                font-size: 9pt;
+            }
+            table {
+                width: 80%;
+                margin-left: auto;
+                margin-right: auto;
+                margin-bottom: 1em;
+            }
+            td {
+                border-bottom: 1px solid gray;
+            }
+            tr:nth-child(2) td {
+                border-top: 1px solid gray;
+            }
+        </style>
+    </head>
+    <body>
+        {{#tags}}
+        <table>
+            <tr><th>{{name}}</th></tr>
+            {{#content}}
+            <tr><td>{{.}}</td></tr>
+            {{/content}}
+        </table>
+        {{/tags}}
+    </body>
+</html>`;
 
 HTMLPreview.create().show(MustacheTemplate.createWithTemplate(template).render(data));
